ee3a20383541024d96c1e81e11fee58c76e46811
sawasana <faithjebichy@gmail.com>
2024-05-06 13:58:24 +0300

    build(book): create book model

---
+++ library-management-system/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+class Book:
+    """This class represents a book model"""
+
+    def __init__(self, isbn, title, author):
+        self.isbn = isbn
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        self.book = dict(isbn=self.isbn, title=self.title, author=self.author)
+        return f"{self.book}"
+
+
+def run():
+    """This function runs the application"""
+
+
+if __name__ == "__main__":
+    run()
