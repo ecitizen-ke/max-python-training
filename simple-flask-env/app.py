0a09bf6a44917eecc8df1e1aa2aa399f0bdc09f4
Kipruto Barno <barxwells@gmail.com>
2024-05-28 15:23:07 +0300

    build(configs): add configs from instance folder

--- simple-flask-env/app.py
+++ simple-flask-env/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
 
-app = Flask(__name__)
-app.config.from_object("settings")
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile("config.py")
+
 
 print(app.config)
 
