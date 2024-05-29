814409c57772c6497a9ce4859f7a1681af827084
Kipruto Barno <barxwells@gmail.com>
2024-05-29 12:50:59 +0300

    build(configs): use external config settings

--- simple-flask-env/app/__init__.py
+++ simple-flask-env/app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 
 def create_app(config_name):
     """This function returns a flask object"""
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_name)
     print(app.config)
+
     return app
--- simple-flask-env/run.py
+++ simple-flask-env/run.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
 from app import create_app
+from instance.config import config
 
+# Automatically load env variables on system startup
+config_name = config.get(os.environ.get("ENV"))
 
-app = create_app()
+app = create_app(config_name)
 
 
 if __name__ == "__main__":
